1e410632caf1495d1ecb22e92409518a08ab540d
Danil Sagajdachnyj <chairowner@yandex.ru>
2024-08-11 21:25:40 +0500

    add https server connection option

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,25 +1,21 @@
 import express, { Express } from "express";
 import authRouter from "./routers/auth.routers";
+import fs from "fs";
 import { sequelize } from "./configs/db.connect.config";
 import { corsMiddleware } from "./middleware/cors.middleware";
 import { setupSocketHandlers } from "./handlers/socketHandlers";
 import { createServer as createHttpServer, Server as HttpServer } from "http";
+import {
+	createServer as createHttpsServer,
+	Server as HttpsServer,
+} from "https";
 import { Server as SocketServer } from "socket.io";
 import "dotenv/config";
 
+const IS_HTTPS: boolean = false;
+
 const app: Express = express();
 const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 3000;
-const SOCKET_PORT: number = process.env.SOCKET_PORT
-	? parseInt(process.env.SOCKET_PORT)
-	: 3001;
-
-const server: HttpServer = createHttpServer(app);
-const io: SocketServer = new SocketServer(server, {
-	cors: {
-		origin: "*",
-		methods: ["GET", "POST"],
-	},
-});
 
 sequelize.authenticate();
 sequelize.sync();
@@ -28,20 +24,47 @@ app.use(corsMiddleware);
 app.use(express.json());
 app.use("/api/auth", authRouter);
 
-try {
-	app.listen(PORT, async () => {
-		console.log(`[SERVER][START][${PORT}]`);
-	});
-} catch (_e) {
-	console.error("[SERVER][START_ERROR]", _e);
-}
+if (IS_HTTPS) {
+	const options = {
+		key: fs.readFileSync("key.pem"),
+		cert: fs.readFileSync("cert.pem"),
+	};
+	const server: HttpsServer = createHttpsServer(options, app);
+	try {
+		server.listen(PORT, async () => {
+			console.log(`[SERVER][START][${PORT}]`);
+		});
+	} catch (_e) {
+		console.error("[SERVER][START_ERROR]", _e);
+	}
+} else {
+	const SOCKET_PORT: number = process.env.SOCKET_PORT
+		? parseInt(process.env.SOCKET_PORT)
+		: 3001;
 
-setupSocketHandlers(io);
-
-try {
-	server.listen(SOCKET_PORT, async () => {
-		console.log(`[SOCKET][START][${SOCKET_PORT}]`);
+	const server: HttpServer = createHttpServer(app);
+	const io: SocketServer = new SocketServer(server, {
+		cors: {
+			origin: "*",
+			methods: ["GET", "POST"],
+		},
 	});
-} catch (_e) {
-	console.error("[SOCKET][START_ERROR]", _e);
+
+	try {
+		app.listen(PORT, async () => {
+			console.log(`[SERVER][START][${PORT}]`);
+		});
+	} catch (_e) {
+		console.error("[SERVER][START_ERROR]", _e);
+	}
+
+	setupSocketHandlers(io);
+
+	try {
+		server.listen(SOCKET_PORT, async () => {
+			console.log(`[SOCKET][START][${SOCKET_PORT}]`);
+		});
+	} catch (_e) {
+		console.error("[SOCKET][START_ERROR]", _e);
+	}
 }
